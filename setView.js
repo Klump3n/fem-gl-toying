fedfef0468f1e76da62dfa0233ab91c2cd3bb3e5
Matthias Plock <matthias.plock@bam.de>
2017-03-02 09:33:56 +0100

    added licenses

--- setView.js
+++ setView.js
@@ -1,9 +1,28 @@
+/*
+ * fem-gl-toying -- getting to terms with WebGL and JavaScript
+ * Copyright (C) 2017 Matthias Plock <matthias.plock@bam.de>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
 /**
  * @fileOverview
  * Contains an object that sets the view matrix and keeps track of mouse-dragging events.
  * @name setView.js
- * @author Matthias Plock
- * @license TBD
+ * @author Matthias Plock <matthias.plock@bam.de>
+ * @license GLPv3
  */
 
 /**
